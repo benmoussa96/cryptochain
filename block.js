c69aeee37c990a4e42d4b3a8e1d34eab1a99a403
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-11-27 17:54:57 +0100

    Adjust the difficulty in mineBlock code

--- block.js
+++ block.js
@@ -24,8 +24,8 @@ class Block {
         do {
             nonce++;
             timestamp = Date.now();
-            difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp })
-            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
+            difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
+            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
         } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
         return new Block({ timestamp, lastHash, data, difficulty, nonce, hash })
@@ -33,6 +33,8 @@ class Block {
 
     static adjustDifficulty({ originalBlock, timestamp }) {
         const { difficulty } = originalBlock;
+
+        if (difficulty < 1) return 1;
 
         if ((timestamp - originalBlock.timestamp) > MINE_RATE) return difficulty - 1;
 
