98a8352d8ddf29a0983ce18af4e3b6595ec71717
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-11-28 14:15:25 +0100

    POST endpoint setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 
 const PORT = 3000;
@@ -6,10 +7,23 @@ const PORT = 3000;
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get(
     '/api/blocks',
     (req, res) => {
         res.json(blockchain.chain);
+    }
+);
+
+app.post(
+    '/api/mine',
+    (req, res) => {
+        const { data } = req.body;
+
+        blockchain.addBlock({ data });
+
+        res.redirect('/api/blocks');
     }
 );
 
