0458c73b00d6f508d1c5290c815dda863013ca10
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-11-30 12:26:23 +0100

    signing data and verifying signatures

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
     let wallet;
@@ -14,4 +15,28 @@ describe('Wallet', () => {
     it('has a `publicKey`', () => {
         expect(wallet).toHaveProperty('publicKey');
     });
-})
+
+    describe('signing data', () => {
+        const data = 'fooBar';
+
+        it('verifies a signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    signature: wallet.sign(data),
+                    data
+                })
+            ).toBe(true);
+        });
+
+        it('does not verifie an invalid signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    signature: new Wallet().sign(data),
+                    data
+                })
+            ).toBe(false);
+        });
+    });
+});
