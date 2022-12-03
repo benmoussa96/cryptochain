d2767fab6216e6351c4a3792ee5ccb41ccc2e160
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-12-03 12:12:00 +0100

    TransactionPool & setTransaction() code

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,6 +1,6 @@
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
-const Wallet = require('./wallet');
+const Wallet = require('./index');
 
 describe('TransactionPool', () => {
     let transactionPool, transaction;
