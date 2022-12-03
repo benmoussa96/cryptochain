a4f86544bd00dc43fa78ce1dfd1d519506696165
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-12-03 12:58:55 +0100

    handling transaction updates

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,10 +3,10 @@ const Transaction = require('./transaction');
 const Wallet = require('./index');
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(() => {
-        const senderWallet = new Wallet();
+        senderWallet = new Wallet();
         transaction = new Transaction({ senderWallet, recipient: 'fake-recipient', amount: 50 });
         transactionPool = new TransactionPool();
     });
@@ -14,7 +14,18 @@ describe('TransactionPool', () => {
     describe('setTransaction()', () => {
         it('adds a transaction', () => {
             transactionPool.setTransaction(transaction);
+
             expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
+        });
+    });
+
+    describe('existingTransaction()', () => {
+        it('returns an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(
+                transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey })
+            ).toBe(transaction);
         });
     });
 })
