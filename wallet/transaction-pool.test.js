d1af7f50b28393262cb011e0a4a48c66ccc4f6db
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-12-03 12:08:40 +0100

    TransactionPool & setTransaction() tests

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,11 +3,18 @@ const Transaction = require('./transaction');
 const Wallet = require('./wallet');
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction, wallet;
+    let transactionPool, transaction;
 
     beforeEach(() => {
-        senderWallet = new Wallet();
+        const senderWallet = new Wallet();
         transaction = new Transaction({ senderWallet, recipient: 'fake-recipient', amount: 50 });
         transactionPool = new TransactionPool();
-    })
+    });
+
+    describe('setTransaction()', () => {
+        it('adds a transaction', () => {
+            transactionPool.setTransaction(transaction);
+            expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
+        });
+    });
 })
