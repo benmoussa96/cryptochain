6f0b975d13c6547210efb59155615690d55572f2
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-12-05 18:05:38 +0100

    fetch and display wallet info

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    state = {
+        walletInfo: { address: '0xfoo', balance: 9999 }
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/wallet-info')
+            .then(response => response.json())
+            .then(data => this.setState({ walletInfo: data }));
+    }
+
     render() {
+        const { address, balance } = this.state.walletInfo;
+
         return (
-            <div>Cryptochain from React App component!</div>
+            <div>
+                <div>Cryptochain from React App component!</div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
+            </div>
         );
     }
 }
