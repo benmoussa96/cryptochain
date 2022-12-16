6b9c0c69a48f10826fec4a210bb889c60edc139e
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-12-16 20:55:53 +0100

    pagination of the blocks in the frontend

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Block from './Block';
 
 class Blocks extends Component {
     state = {
-        blocks: []
+        blocks: [],
+        blocksLength: 0,
+        paginatedId: 1
     };
 
     componentDidMount() {
-        fetch(`${document.location.origin}/api/blocks`)
+        fetch(`${document.location.origin}/api/blocks/length`)
+            .then(response => response.json())
+            .then(data => this.setState({ blocksLength: data }));
+
+        this.fetchPaginatedBlocks(this.state.paginatedId)();
+    }
+
+    fetchPaginatedBlocks = paginatedId => () => {
+        fetch(`${document.location.origin}/api/blocks/${paginatedId}`)
             .then(response => response.json())
             .then(data => this.setState({ blocks: data }));
     }
@@ -18,6 +29,21 @@ class Blocks extends Component {
             <div className='Blocks'>
                 <div><Link to='/'>Home</Link></div>
                 <h3>Blocks</h3>
+                <div>
+                    {
+                        [...Array(Math.ceil(this.state.blocksLength / 5)).keys()].map(key => {
+                            const paginatedId = key + 1;
+
+                            return (
+                                <span key={key} onClick={this.fetchPaginatedBlocks(paginatedId)}>
+                                    <Button bsSize="small" bsStyle="danger">
+                                        {paginatedId}
+                                    </Button>{' '}
+                                </span>
+                            )
+                        })
+                    }
+                </div>
                 {
                     this.state.blocks.map(block => {
                         return (
